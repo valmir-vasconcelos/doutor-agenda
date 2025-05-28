a88e74709ffa2fce7e801e30df4b4744577e4462
Valmir Vasconcelos de Araujo <valmir.vasconcelos@parauapebas.pa.gov.br>
2025-05-28 13:08:42 -0300

    feat: implement session management and redirection in authentication and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,28 @@
-const DashboardPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
-            <p className="text-lg">Welcome to your dashboard!</p>
-        </div>
-    );
-}
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-4xl font-bold">Dashboard</h1>
+      <p className="text-lg">Welcome to your dashboard!</p>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      <SignOutButton />
+    </div>
+  );
+};
 
 export default DashboardPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt_br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
