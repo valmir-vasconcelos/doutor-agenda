7a81792ffefc89f462bc5ce17c2ae2c1e14254d3
Valmir Vasconcelos de Araujo <valmir.vasconcelos@parauapebas.pa.gov.br>
2025-05-28 14:20:35 -0300

    creating clinic form

---
+++ src/app/clinic-form/components/form.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { DialogFooter } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const clinicFormSchema = z.object({
+  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+});
+
+const ClinicForm = () => {
+  const form = useForm<z.infer<typeof clinicFormSchema>>({
+    resolver: zodResolver(clinicFormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof clinicFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+      <DialogFooter>
+        <Button type="submit">Criar clínica</Button>
+      </DialogFooter>
+    </Form>
+  );
+};
+
+export default ClinicForm;
