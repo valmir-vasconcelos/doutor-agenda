6d42f840d45c9efdb45e34034c445e2c3fd3dd4f
Valmir Vasconcelos <valmir.bsi@gmail.com>
2025-05-27 21:48:11 -0300

    feat: add sign up form

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,48 +1,10 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Card, CardDescription, CardHeader, CardTitle, } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-const registerSchema = z.object({
-  name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
-  email: z.string().trim().min(1, { message: "E-mail é obrigatório" }),
-  password: z.string().trim().min(8, { message: "Senha é obrigatória" }),
-});
+import SignUpForm from "./components/sign-up-form";
 
 const AuthenticationPage = () => {
-  const form = useForm<z.infer<typeof registerSchema>>({
-    resolver: zodResolver(registerSchema),
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof registerSchema>) {
-    console.log(values);
-  }
 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
@@ -60,39 +22,7 @@ const AuthenticationPage = () => {
           </Card>
         </TabsContent>
         <TabsContent value="register">
-          <Card>
-            <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8"
-              >
-                <CardHeader>
-                  <CardTitle>Criar conta</CardTitle>
-                  <CardDescription>
-                    Crie uma conta para continuar
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Nome</FormLabel>
-                        <FormControl>
-                          <Input placeholder="Digite seu nome" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </CardContent>
-                <CardFooter>
-                  <Button type="submit">Submit</Button>
-                </CardFooter>
-              </form>
-            </Form>
-          </Card>
+          <SignUpForm />
         </TabsContent>
       </Tabs>
     </div>
